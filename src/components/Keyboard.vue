d5d136bf406a21c6fe2d04921290f0042e14bdf2
HesamAhr <hesamya.ahrari@gmail.com>
2022-01-27 16:38:37 +0330

    add initial dependency and component

---
+++ src/components/Keyboard.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="d-flex justify-content-center align-items-center">
+    <div v-for="char in keyboard" :key="char" class="p-2 border" @click="keydown(char)">
+      <div>
+        {{ char }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Keyboard",
+  data() {
+    return {
+      keyboard: [
+        "q",
+        "w",
+        "e",
+        "r",
+        "t",
+        "y",
+        "u",
+        "i",
+        "o",
+        "p",
+        "a",
+        "s",
+        "d",
+        "f",
+        "g",
+        "h",
+        "j",
+        "k",
+        "l",
+        "z",
+        "x",
+        "c",
+        "v",
+        "b",
+        "n",
+        "m",
+        "delete",
+        "enter",
+      ],
+    };
+  },
+  methods: {
+    keydown(e) {
+      // if (e === 'enter') {
+			// 	this.$emit('enter');
+			// }
+			// else if (e === 'delete') {
+			// 	this.$emit('delete');
+			// }
+			// else {
+				this.$emit('input', e);
+			// }
+    },
+  },
+};
+</script>
+
+<style></style>
